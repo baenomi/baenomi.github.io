0f641262cf539241fa0ba854c22fa405a6db73b2
Bartosz <152443101+baenomi@users.noreply.github.com>
2024-07-26 16:47:05 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const project = document.querySelector('.projects-container');
 const socials = document.querySelector('.socials');
 const additionalInfo = document.querySelector('.additional-info');
 const menu = document.querySelector('.menu');
+const projectCards = document.querySelectorAll('.project-card');
+const contact = document.querySelector('.contact-card');
+const contactTitle = document.querySelector('.contact-container');
+const footer = document.querySelector('.footer');
+const navbar = document.querySelector('.top');
+
 
 function setIcon(isDarkMode) {
     const isMobile = window.matchMedia('(max-width: 600px)').matches;
@@ -14,17 +20,20 @@ function setIcon(isDarkMode) {
     toggleIcon.src = isDarkMode
         ? (isMobile ? 'images/moon.png' : 'images/moon.png')
         : (isMobile ? 'images/sun-mobile.png' : 'images/sun-mobile.png');
-    
 }
 
-if (localStorage.getItem('dark-mode') === 'enabled') {
+if (localStorage.getItem('dark-mode') === 'disabled') {
     document.body.classList.add('dark-mode');
     intro.classList.add('dark-mode');
     intro2.classList.add('dark-mode');
     socials.classList.add('dark-mode');
     additionalInfo.classList.add('dark-mode');
     menu.classList.add('dark-mode');
-    project.classList.add('dark-mode');
+    contact.classList.add('dark-mode');
+    contactTitle.classList.add('dark-mode');
+    footer.classList.add('dark-mode');
+    navbar.classList.add('dark-mode');
+    projectCards.forEach(card => card.classList.add('dark-mode'));
     setIcon(true);
 } else {
     setIcon(false);
@@ -37,7 +46,11 @@ toggleButton.addEventListener('click', () => {
     socials.classList.toggle('dark-mode', isDarkMode);
     additionalInfo.classList.toggle('dark-mode', isDarkMode);
     menu.classList.toggle('dark-mode', isDarkMode);
-    project.classList.toggle('dark-mode', isDarkMode);
+    contact.classList.toggle('dark-mode', isDarkMode);
+    contactTitle.classList.toggle('dark-mode', isDarkMode);
+    footer.classList.toggle('dark-mode', isDarkMode);
+    navbar.classList.toggle('dark-mode', isDarkMode);
+    projectCards.forEach(card => card.classList.toggle('dark-mode', isDarkMode));
     toggleIcon.style.opacity = 1;
 
     setTimeout(() => {
@@ -49,36 +62,13 @@ toggleButton.addEventListener('click', () => {
 });
 
 
+
 function handleResize() {
     const isDarkMode = document.body.classList.contains('dark-mode');
     setIcon(isDarkMode);
 }
 
 window.addEventListener('resize', handleResize);
-
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
-
-document.querySelectorAll('.menu-item').forEach(item => {
-    item.addEventListener('click', event => {
-        event.preventDefault();
-        const sectionId = item.getAttribute('href').substring(1);
-        const section = document.getElementById(sectionId);
-        section.scrollIntoView({ behavior: 'smooth' });
-    });
-});
 
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.getElementById('menu-toggle');
@@ -116,7 +106,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// Function to show or hide the scroll-to-top button
 function toggleScrollToTopButton() {
     const scrollToTopButton = document.querySelector('.scroll-to-top');
     const scrollThreshold = window.innerWidth <= 768 ? 1400 : 800;
@@ -128,10 +117,8 @@ function toggleScrollToTopButton() {
     }
 }
 
-// Event listener for scrolling
 window.addEventListener('scroll', toggleScrollToTopButton);
 
-// Event listener for click on the scroll-to-top button
 document.querySelector('.scroll-to-top').addEventListener('click', function(event) {
     event.preventDefault();
     window.scrollTo({
@@ -139,3 +126,36 @@ document.querySelector('.scroll-to-top').addEventListener('click', function(even
         behavior: 'smooth'
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    const isMobile = window.matchMedia('(max-width: 768px)').matches;
+
+    const animateSections = () => {
+        sections.forEach(section => {
+            const top = window.scrollY;
+            const offset = section.offsetTop - (isMobile ? 200 : 150);
+            const height = section.offsetHeight;
+
+            if (top >= offset && top < offset + height) {
+                section.classList.add('show-animate');
+            } else {
+                section.classList.remove('show-animate');
+            }
+        });
+    };
+
+    animateSections();
+
+    window.onscroll = animateSections;
+});
+
+
+
+
+
+
+
+  
+  
+  
